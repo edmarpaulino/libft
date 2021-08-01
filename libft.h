71effd459d6cca033850328c424c1bcf01e852c7
Edmar Paulino <edmarpaulino9@gmail.com>
2021-08-01 11:05:47 -0300

    finished ft_putstr_fd

--- libft.h
+++ libft.h
@@ -19,6 +19,7 @@ int		ft_atoi(const char	*str);
 void	ft_bzero(void	*s, size_t	n);
 void	ft_striteri(char	*s, void	(*f)(unsigned int, char*));
 void	ft_putchar_fd(char	c, int	fd);
+void	ft_putstr_fd(char	*s, int	fd);
 char	*ft_strchr(const char	*s, int	c);
 char	*ft_strrchr(const char	*s, int	c);
 char	*ft_strnstr(const char	*haystack, const char	*needle, size_t	len);
